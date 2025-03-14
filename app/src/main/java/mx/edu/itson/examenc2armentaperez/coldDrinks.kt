9883bf57bec26dd41b13ba3fcbb2c27dc824ba48
CharlyOwo81 <gamaliel.armenta245254@potros.itson.edu.mx>
2025-03-13 18:57:50 -0700

    Tercer Commit

--- app/src/main/java/mx/edu/itson/examenc2armentaperez/coldDrinks.kt
+++ app/src/main/java/mx/edu/itson/examenc2armentaperez/coldDrinks.kt
@@ -1,24 +1,16 @@
 package mx.edu.itson.examenc2armentaperez
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class coldDrinks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val button: Button = findViewById(R.id.boton)
-
-        button.setOnClickListener {
-            var intent: Intent = Intent(this, OptionMenu::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_cold_drinks)
+        
     }
 }
